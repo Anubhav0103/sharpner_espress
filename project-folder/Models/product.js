b359e50e397f4dbabb6ef59e965410cab94d0508
Anubhav0103 <62379559+Anubhav0103@users.noreply.github.com>
2024-08-31 03:33:28 +0530

    Add files via upload

---
+++ project-folder/Models/product.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
+
+const writeProductsToFile = (products, cb) => {
+    fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) {
+            console.log(err);
+        }
+        if (cb) {
+            cb();
+        }
+    });
+};
+
+module.exports = class Product {
+    constructor(id, title, price, description) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+
+    save() {
+        this.id = Math.random().toString();
+        getProductsFromFile((products) => {
+            products.push(this);
+            writeProductsToFile(products);
+        });
+    }
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile((products) => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
+    }
+
+    static deleteProductById(id, cb) {
+        getProductsFromFile((products) => {
+            const updatedProducts = products.filter(p => p.id !== id);
+            writeProductsToFile(updatedProducts, cb);
+        });
+    }
+};
